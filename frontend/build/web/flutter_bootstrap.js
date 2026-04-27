7aefa97758c335fda5c9b9bcd90fd663ef11a9e0
Rabecca Ganggan <gangganrabecca719@gmail.com>
2026-04-27 11:26:07 +0800

    skye: verified fresh build with logo and production url

--- frontend/build/web/flutter_bootstrap.js
+++ frontend/build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1470351339"
+    serviceWorkerVersion: "1880864640"
   }
 });
